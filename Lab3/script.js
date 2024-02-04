8baaddec9feacf38c11befb5af676ee1b70f4973
Voldi <vmonz093@uottawa.com>
2024-02-04 12:08:25 -0500

    moveLeft and moveRight functions completed + index on player stored

--- Lab3/script.js
+++ Lab3/script.js
@@ -1,7 +1,10 @@
 const game = {
     board : [],
-    player : "C",
-    ghost : "^.",
+    player : {
+        sprite : "C",
+        index : 0
+    },
+    ghost : "^",
     fruit : "@",
     dots : "."
 };
@@ -15,10 +18,39 @@ function createGame(n) {
         game.board[i] = game.dots;
     }
     game.board[getRandomInt(game.board.length)] = game.fruit;
-    game.board[getRandomInt(game.board.length)] = game.ghost;
-    game.board[parseInt(n/2)] = game.player;
+    game.board[getRandomInt(game.board.length)] += game.ghost;
+    game.board[game.player.index = parseInt(n/2)] = game.player.sprite;
     document.writeln("Game Created");
     return game.board
 }
 
+function moveLeft(board) {
+    board[game.player.index] -= game.player.sprite;
+    board[game.player.index] = ""
+
+    if (game.player.index == 0) {
+        game.player.index = parseInt(board.length)-1;
+        board[game.player.index] += game.player.sprite;
+    }else{
+        game.player.index = parseInt(game.player.index)-1;
+        board[game.player.index] += game.player.sprite;
+    }
+    return board
+}
+
+function moveRight(board) {
+    board[game.player.index] -= game.player.sprite;
+    board[game.player.index] = ""
+
+    if (game.player.index == board.length - 1) {
+        game.player.index = 0;
+        board[0] += game.player.sprite;
+    }else{
+        game.player.index = parseInt(game.player.index)+1
+        board[game.player.index] += game.player.sprite;
+    }
+    return board
+}
+
 document.writeln(createGame(8));
+document.writeln(moveLeft(game.board));
