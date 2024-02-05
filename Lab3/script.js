68036fed7af65aa63e400f8d93213ab6fb382072
Voldi <vmonz093@uottawa.com>
2024-02-05 05:46:48 -0500

    UI implementation V1 + Game model imporvement

--- Lab3/script.js
+++ Lab3/script.js
@@ -4,10 +4,14 @@ const game = {
         sprite : "C",
         index : 0
     },
-    ghost : "^",
+    ghost : {
+        sprite : "^",
+        index : 0
+    },
     fruit : "@",
     dots : ".",
-    score : 0
+    score : 0,
+    isGameOver : false,
 };
 
 function getRandomInt(max) {
@@ -19,53 +23,160 @@ function createGame(n) {
         game.board[i] = game.dots;
     }
     game.board[getRandomInt(game.board.length)] = game.fruit;
-    game.board[getRandomInt(game.board.length)] += game.ghost;
+    game.board[game.ghost.index = getRandomInt(game.board.length)] += game.ghost.sprite;
     game.board[game.player.index = parseInt(n/2)] = game.player.sprite;
-    document.writeln("Game Created");
+    display(game.board);    
     return game.board
 }
 
-function moveLeft(board) {
-    board[game.player.index] -= game.player.sprite;
-    board[game.player.index] = ""
+function moveLeft(user, board) {
+    board[user.index] -= user.sprite;
+    if(user.sprite.includes(game.player.sprite) || isNaN(board[user.index]))
+        board[user.index] = "";
 
-    if (game.player.index == 0) {
-        game.player.index = parseInt(board.length)-1;
-        if(board[game.player.index].includes(game.dots)){
+
+    //Gestion des mouvement 
+    if (user.index == 0) {
+        user.index = parseInt(board.length)-1;
+        //Gestion du score
+        if(board[user.index].includes(game.dots) && user.sprite == game.player.sprite){
             game.score++;
         }
-        board[game.player.index] += game.player.sprite;
+        //Gestion des Fantome
+        if(board[user.index].includes(game.ghost.sprite) && user.sprite == game.player.sprite){
+            gameOver(game);
+        }
+        board[user.index] = user.sprite;
     }else{
-        game.player.index = parseInt(game.player.index)-1;
-        if(board[game.player.index].includes(game.dots)){
+        user.index = parseInt(user.index)-1;
+        //Gestion du score
+        if(board[user.index].includes(game.dots) && user.sprite == game.player.sprite){
             game.score++;
         }
-        board[game.player.index] += game.player.sprite;
+        //Gestion du Fantome
+        if(board[user.index].includes(game.ghost.sprite) && user.sprite == game.player.sprite){
+            gameOver(game);
+        }
+        board[user.index] = user.sprite;
     }
-    document.writeln("score : ", game.score);
+    update(board);
+    nextLevel(game);
     return board
 }
 
-function moveRight(board) {
-    board[game.player.index] -= game.player.sprite;
-    board[game.player.index] = ""
+function moveRight(user, board) {
+    board[user.index] -= user.sprite;
+    if(user.sprite.includes(game.player.sprite) || isNaN(board[user.index]))
+        board[user.index] = "";
 
-    if (game.player.index == board.length - 1) {
-        if(board[0].includes(game.dots)){
+    if (user.index == board.length - 1) {
+        user.index = 0;
+        //Gestion du score
+        if(board[0].includes(game.dots) && user.sprite == game.player.sprite){
             game.score++;
         }
-        game.player.index = 0;
-        board[0] += game.player.sprite;
+        //Gestion des Fantome
+        if(board[user.index].includes(game.ghost.sprite) && user.sprite == game.player.sprite){
+            gameOver(game);
+        }
+        board[0] = user.sprite;
     }else{
-        game.player.index = parseInt(game.player.index)+1
-        if(board[game.player.index].includes(game.dots)){
+        user.index = parseInt(user.index)+1
+        //Gestion du score
+        if(board[user.index].includes(game.dots) && user.sprite == game.player.sprite){
             game.score++;
         }
-        board[game.player.index] += game.player.sprite;
+        //Gestion des Fantome
+        if(board[user.index].includes(game.ghost.sprite) && user.sprite == game.player.sprite){
+            gameOver(game);
+        }
+        board[user.index] = user.sprite;
     }
-    document.writeln("score : ", game.score);
+    update(board);
+    nextLevel(game);
     return board
 }
 
-document.writeln(createGame(8));
-document.writeln(moveLeft(game.board));
+function nextLevel(g){
+    //Verification if we should advance to the next level
+    for(i of g.board){
+        if(i.includes(game.dots)){
+            return false;
+        }
+    }
+
+    //Advancing to the next level
+    //regenerating the game but in a bigger size
+    return true;
+}
+
+function display(board) {
+    var boardElements = [];
+    for(i in board){
+        boardElements[i] = document.createElement("div");
+        boardElements[i].className = "cell elem "+ i;
+        boardElements[i].id = "cell elem "+ i;
+        boardElements[i].innerHTML = board[i];
+        document.getElementById("board").appendChild(boardElements[i]);
+    }
+}
+
+function update(board) {
+    var boardElements = [];
+    for(i in board){
+        boardElements[i] = document.getElementById("cell elem "+ i)
+        if(boardElements[i] != null)
+            boardElements[i].innerHTML = board[i];
+    }
+}
+
+function deleteGame(){
+    const list = document.getElementById("board");
+
+    while (list.hasChildNodes()) {
+        list.removeChild(list.firstChild);
+      }
+}
+
+function gameOver(g) {
+    g.isGameOver = true;
+    document.getElementById("game-status").innerHTML = "GAME OVER";
+}
+
+function restart(g,n) {
+    g.board = [];
+    g.score = 0;
+    g.player.index = 0;
+    g.isGameOver = false;
+    deleteGame();
+    createGame(n);
+    document.getElementById("game-status").innerHTML = ""
+    return g.board;
+}
+
+createGame(7)
+
+// HTML button elements
+const btnMoveLeft = document.getElementById("moveLeft");
+const btnMoveRight = document.getElementById("moveRight");
+const btnRestart = document.getElementById("restartButton");
+
+// btnMoveLeft click event listener to apply function
+btnMoveLeft.addEventListener("click", function() {
+    // your function code here
+    if(!game.isGameOver)
+        moveLeft(game.player,game.board);
+});
+
+// btnMoveRight click event listener to apply function
+btnMoveRight.addEventListener("click", function() {
+    // your function code here
+    if(!game.isGameOver)
+        moveRight(game.player,game.board);
+});
+
+// btnMoveLeft click event listener to apply function
+btnRestart.addEventListener("click", function() {
+    // your function code here
+    restart(game,7);
+});
